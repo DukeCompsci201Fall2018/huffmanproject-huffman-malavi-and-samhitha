125d7be1f78f47654c963ce535c5cda14ce6ceb9
Malavi Ravindran <malavithemanatee@gmail.com>
2018-12-02 14:53:56 -0500

    'work'

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -60,12 +60,23 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE) {
+			throw new HuffException("illegal heder starts with "+bits);
 		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedbits(root,in,out);
 		out.close();
+		//while (true){
+			//int val = in.readBits(BITS_PER_WORD);
+		//	if (val == -1) break;
+		//	out.writeBits(BITS_PER_WORD, val);
+		//}
+		//out.close();
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
